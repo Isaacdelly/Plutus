4c813adc6ebd9014eda163526655a0673024ad33
Isaacdelly <isaacdelly@gmail.com>
2018-10-31 07:47:27 -0700

    removed unused imports (sys, time, threading)

--- plutus.py
+++ plutus.py
@@ -3,15 +3,12 @@
 # https://github.com/Isaacdelly/Plutus
 
 try:
-    import sys
     import os
-    import time
     import hashlib
     import binascii
     import multiprocessing
     from multiprocessing import Process, Queue
     from multiprocessing.pool import ThreadPool
-    import threading
     import base58
     import ecdsa
     import requests
